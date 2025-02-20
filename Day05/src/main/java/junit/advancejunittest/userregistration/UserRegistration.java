3b33f5202bbde62a4d39e07a25a369d72d9fda50
Kuldeep <sharmakuldeep74420@gmail.com>
2025-02-20 11:33:45 +0530

    [Kuldeep sharma] add. Validate user registration number

---
+++ Day05/src/main/java/junit/advancejunittest/userregistration/UserRegistration.java
@@ -0,0 +1,10 @@
+package junit.advancejunittest.userregistration;
+
+public class UserRegistration {
+    public static boolean registerUser(String username, String email, String password) {
+        if (username.isEmpty() || email.isEmpty() || password.length() < 8) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        return true;
+    }
+}
